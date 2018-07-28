e81c804ab49bd44aed5d0dbd1cb7ab7e786aebbb
Mitch PC <cartwright1993@live.com>
2018-07-28 16:16:05 +0100

    Added Music for Oliv3's campaign

--- CfgMusic.hpp
+++ CfgMusic.hpp
@@ -24,4 +24,19 @@ class CfgMusic
 		name	= "IVowToThee";
 		sound[]	= { "LRG Fundamentals\Media\Music\IVowToThee.ogg", db + 0, 1.0 };
 	};	
+	class CalonLan
+	{
+		name	= "CalonLan";
+		sound[]	= { "LRG Fundamentals\Media\Music\CalonLan.ogg", db + 0, 1.0 };
+	};
+	class BandofBrothers
+	{
+		name	= "BandofBrothers";
+		sound[]	= { "LRG Fundamentals\Media\Music\BandofBrothers.ogg", db + 0, 1.0 };
+	};
+	class BohemianRhapsody
+	{
+		name	= "BohemianRhapsody";
+		sound[]	= { "LRG Fundamentals\Media\Music\BohemianRhapsody.ogg", db + 0, 1.0 };
+	};	
 };
