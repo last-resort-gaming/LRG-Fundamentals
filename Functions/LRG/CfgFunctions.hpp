cd782716c91857e755078fe58aed47b14c25569d
Mokka <mokkamichi.99@gmail.com>
2018-11-06 22:55:34 +0100

    fn_HandleSetupPostInit added to handle new onPostInit functionality
    Needs some testing before deployment, but should be fine since there isn't a lot of code going on...

--- Functions/LRG/CfgFunctions.hpp
+++ Functions/LRG/CfgFunctions.hpp
@@ -78,8 +78,49 @@ class LR {
 			
 			class Training {
 
-				file = "LRG Fundamentals\Functions\LRG\Training";
-				class Casualty {};
-				class ResetTargets {};
-			};
+		file = "LRG Fundamentals\Functions\LRG\Interaction";
+		class DynamicText {};
+		class InitInspectableIntel {};
+		class LaptopHoldActions {};
+		class PlayerAddActions {};
+		class RunIntelAction {};
+		class SetIntelAction {};
+		class AddCreatorAction {};
+		class AddAction {};
+		class AddSelfAction {};
+		class AddHoldAction {};
+	};
+
+	class PostInit {
+
+		file = "LRG Fundamentals\Functions\LRG\PostInit";
+		class AISpawns {postinit = 1;};
+		class ArsenalLoadSave {postInit = 1;};
+		class Diary {postInit = 1;};
+		class DynamicSim { postInit = 1; };
+		class InitPlayerLocal {postInit = 1;};
+		class InitServer {postInit = 1;};
+		class Logs {postInit = 1;};
+		class PilotCheck {postInit = 1;};
+		class RealWeather {postInit = 1;};
+		class SafeZone {postInit = 1;};
+		class SetInitialAI {postInit = 1;};
+		class SideChat {postInit = 1;};
+		class InitCreatorActions {postInit = 1;};
+		class AddCueCards {postInit = 1;};
+		class HandleSetupPostInit {postInit = 1;};
+	};
+
+	class PreInit {
+
+		file = "LRG Fundamentals\Functions\LRG\PreInit";
+		class PreInit {preInit = 1;};
+	};
+
+	class Training {
+
+		file = "LRG Fundamentals\Functions\LRG\Training";
+		class Casualty {};
+		class ResetTargets {};
+	};
 };
