b509c66039c5bc1ecb2966643712331a1670c533
Mitch <cartwright1993@live.com>
2018-11-12 13:44:16 +0000

    Merge Music and Sound

--- CfgMusic.hpp
+++ CfgMusic.hpp
@@ -1,42 +1,76 @@
 class CfgMusic
 {
 	tracks[] = {};
+	class Denied
+	{
+		name		= "Denied";
+		sound[]		= { "LRG Fundamentals\Media\Sounds\Denied.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
+	};
+
+	class AirRaid
+	{
+		name		= "AirRaid";
+		sound[]		= { "LRG Fundamentals\Media\Sounds\AirRaid.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
+	};
+	
+	class NukeAlarm
+	{
+		name		= "NukeAlarm";
+		sound[]		= { "LRG Fundamentals\Media\Sounds\NukeAlarm.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
+	};
+
+	class Windows
+	{
+		name		= "Windows";
+		sound[]		= { "LRG Fundamentals\Media\Sounds\Windows.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
+	};
 	class Jerusalem
 	{
 		name	= "Jerusalem";
-		sound[]	= { "LRG Fundamentals\Media\Music\Jerusalem.ogg", db + 0, 1.0 };
+		sound[]	= { "LRG Fundamentals\Media\Music\Jerusalem.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
 	};
 	
 	class RuleBritannia
 	{
 		name	= "RuleBritannia";
-		sound[]	= { "LRG Fundamentals\Media\Music\RuleBritannia.ogg", db + 0, 1.0 };
+		sound[]	= { "LRG Fundamentals\Media\Music\RuleBritannia.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
 	};	
 
 	class BlackHawkDown
 	{
 		name	= "BlackHawkDown";
-		sound[]	= { "LRG Fundamentals\Media\Music\BlackHawkDown.ogg", db + 0, 1.0 };
+		sound[]	= { "LRG Fundamentals\Media\Music\BlackHawkDown.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
 	};
 	
 	class IVowToThee
 	{
 		name	= "IVowToThee";
-		sound[]	= { "LRG Fundamentals\Media\Music\IVowToThee.ogg", db + 0, 1.0 };
+		sound[]	= { "LRG Fundamentals\Media\Music\IVowToThee.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
 	};	
 	class CalonLan
 	{
 		name	= "CalonLan";
-		sound[]	= { "LRG Fundamentals\Media\Music\CalonLan.ogg", db + 0, 1.0 };
+		sound[]	= { "LRG Fundamentals\Media\Music\CalonLan.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
 	};
 	class BandofBrothers
 	{
 		name	= "BandofBrothers";
-		sound[]	= { "LRG Fundamentals\Media\Music\BandofBrothers.ogg", db + 0, 1.0 };
+		sound[]	= { "LRG Fundamentals\Media\Music\BandofBrothers.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
 	};
 	class BohemianRhapsody
 	{
 		name	= "BohemianRhapsody";
-		sound[]	= { "LRG Fundamentals\Media\Music\BohemianRhapsody.ogg", db + 0, 1.0 };
+		sound[]	= { "LRG Fundamentals\Media\Music\BohemianRhapsody.ogg", 1, 1, 100 };
+		titles[]	= { 0, "" };
 	};	
 };
