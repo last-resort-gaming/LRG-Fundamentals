fef8ad0334c6a800ef721f230d9b2b8054716bc1
Mitch <cartwright1993@live.com>
2019-01-07 13:48:48 +0000

    Media Cfgs updated.

--- Media/CfgMusic.hpp
+++ Media/CfgMusic.hpp
@@ -28,49 +28,4 @@ class CfgMusic
 		sound[]		= { "LRG Fundamentals\Media\Sounds\Windows.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
-	class Jerusalem
-	{
-		name	= "Jerusalem";
-		sound[]	= { "LRG Fundamentals\Media\Music\Jerusalem.ogg", 1, 1, 100 };
-		titles[]	= { 0, "" };
-	};
-	
-	class RuleBritannia
-	{
-		name	= "RuleBritannia";
-		sound[]	= { "LRG Fundamentals\Media\Music\RuleBritannia.ogg", 1, 1, 100 };
-		titles[]	= { 0, "" };
-	};	
-
-	class BlackHawkDown
-	{
-		name	= "BlackHawkDown";
-		sound[]	= { "LRG Fundamentals\Media\Music\BlackHawkDown.ogg", 1, 1, 100 };
-		titles[]	= { 0, "" };
-	};
-	
-	class IVowToThee
-	{
-		name	= "IVowToThee";
-		sound[]	= { "LRG Fundamentals\Media\Music\IVowToThee.ogg", 1, 1, 100 };
-		titles[]	= { 0, "" };
-	};	
-	class CalonLan
-	{
-		name	= "CalonLan";
-		sound[]	= { "LRG Fundamentals\Media\Music\CalonLan.ogg", 1, 1, 100 };
-		titles[]	= { 0, "" };
-	};
-	class BandofBrothers
-	{
-		name	= "BandofBrothers";
-		sound[]	= { "LRG Fundamentals\Media\Music\BandofBrothers.ogg", 1, 1, 100 };
-		titles[]	= { 0, "" };
-	};
-	class BohemianRhapsody
-	{
-		name	= "BohemianRhapsody";
-		sound[]	= { "LRG Fundamentals\Media\Music\BohemianRhapsody.ogg", 1, 1, 100 };
-		titles[]	= { 0, "" };
-	};	
 };
