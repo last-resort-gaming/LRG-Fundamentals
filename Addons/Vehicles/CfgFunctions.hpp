967d5779683994c1b51a51c54bf4b3119c411096
Mitch <cartwright1993@live.com>
2019-01-19 04:28:11 +0000

    Repair Scripts moved to Vehicles, ready for Module (WIP)

---
+++ Addons/Vehicles/CfgFunctions.hpp
@@ -0,0 +1,9 @@
+class LRG_Vehicles {
+
+	tag = "LR";
+	class Vehicles {
+
+		file = "z\LRG Fundamentals\Addons\Vehicles\Functions";
+		class ConfigEquipment {};
+	};
+};
---
+++ Addons/Vehicles/script_component.hpp
@@ -0,0 +1,3 @@
+#define COMPONENT VEHICLES
+
+#include "\z\LRG Fundamentals\addons\main\script_mod.hpp"
