8bee5a7cd1db15415d6022cac1f6d2bc7ca18202
Mitch <cartwright1993@live.com>
2019-01-19 03:54:18 +0000

    Added Display Name for CfgRespawnTemplates

--- Addons/General/CfgRespawnTemplates.hpp
+++ Addons/General/CfgRespawnTemplates.hpp
@@ -2,7 +2,7 @@ class CfgRespawnTemplates
 {
 	class LRG_RespawnOnStartPosition
 	{
-
+		displayName = "LRG Respawn on Start Position";
 		onPlayerRespawn = "z\LRG Fundamentals\addons\General\Scripts\PlayerRespawn.sqf";
 		respawnDelay = 10;
 		respawnOnStart = 0;
