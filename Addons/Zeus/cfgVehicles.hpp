ffcfea464d58f1b90f04b0ac13f169f866d49a59
Mitch <cartwright1993@live.com>
2019-01-20 06:59:10 +0000

    ConfigZeus Attribute fixed in SP.

--- Addons/Zeus/cfgVehicles.hpp
+++ Addons/Zeus/cfgVehicles.hpp
@@ -1,14 +1,16 @@
-class CfgVehicles
-{
-	class ModuleCurator_F;
-	class MyEntity: ModuleCurator_F
-	{
+
+
+class CfgVehicles {
+    class Module_F;
+    class ModuleEmpty_F;
+
+    class ModuleCurator_F: Module_F {
 		class Attributes
 		{
             class LRG_Config_Zeus {
                 property = QUOTE(LRG_Config_Zeus);
                 control = "Checkbox";
-                displayName = "LRG Config Zeus:";
+                displayName = "LRG Fundamentals: Config Zeus:";
                 tooltip = "Enable to allow this Zeus to have placed units use LRG AI Difficulty Settings (Settings - Addon Options)";
                 expression = "[_this] call LR_fnc_ConfigZeus;";
                 typeName = "BOOL";
