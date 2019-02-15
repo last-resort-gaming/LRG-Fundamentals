ec88a98a9a4c4278bc84dc1740190a5c161a4e12
Mitch <cartwright1993@live.com>
2019-02-15 20:48:03 +0000

    Servicing Module Rewrite

--- Addons/Vehicles/ServicingModule.hpp
+++ Addons/Vehicles/ServicingModule.hpp
@@ -1,94 +1,30 @@
-	class LRG_ModuleServicing: Module_F
+class LRG_ModuleServicing: Module_F
+{
+	scope = 2;
+	displayName = "Vehicle Servicing";
+	icon = "\z\LRG Fundamentals\addons\media\images\icons\Vehicle Service.paa";
+	category = "LRG_Modules";
+	function = "LR_fnc_moduleServicing";
+	functionPriority = 4;
+	isGlobal = 1;
+	isTriggerActivated = 0;
+	isDisposable = 0;
+	is3DEN = 0;
+	class Attributes: AttributesBase
 	{
-		scope = 2;
-		displayName = "Vehicle Servicing";
-		icon = "\z\LRG Fundamentals\addons\media\images\icons\Vehicle Service.paa";
-		author = "MitchJC";
-		vehicleClass = "Modules";
-		category = "LRG_Modules";
-		function = "LR_fnc_moduleServicing";
-		functionPriority = 4;
-		isGlobal = 1;
-		isTriggerActivated = 0;
-		isDisposable = 0;
-		is3DEN = 0;
-		class Arguments: ArgumentsBaseUnits
-		{
-			class ServicingArea
-			{
-				displayName = "Servicing Area";
-				description = "2D Servicing Area Size around this module.";
-				typeName = "NUMBER";
-				defaultValue = 10;
-			};
-			class ServicingHeight
-			{
-				displayName = "Servicing Height";
-				description = "Max Height this area can be activated.";
-				typeName = "NUMBER";
-				defaultValue = 6;
-			};
-/*			class ServicingRearm
-			{
-				displayName = "Rearm Time";
-				description = "How long to Rearm at this module.";
-				typeName = "NUMBER";
-				defaultValue = 20;
-			};
-			class ServicingRefuel
-			{
-				displayName = "Refuel Time";
-				description = "How long to Refuel at this module.";
-				typeName = "NUMBER";
-				defaultValue = 20;
-			};
-			class ServicingRepair
-			{
-				displayName = "Repair Time";
-				description = "How long to Repair at this module.";
-				typeName = "NUMBER";
-				defaultValue = 20;
-			};
-			class ServicingGroundVehicles
-			{
-				displayName = "Ground Vehicles";
-				description = "Can Ground Vehicles be Serviced here?";
-				typeName = "BOOL";
-				defaultValue = 0;
-			};
-			class ServicingHelicopters
-			{
-				displayName = "Helicopters";
-				description = "Can Helicopters be Serviced here?";
-				typeName = "BOOL";
-				defaultValue = 0;
-			};
-			class ServicingPlanes
-			{
-				displayName = "Planes";
-				description = "Can Planes be Serviced here?";
-				typeName = "BOOL";
-				defaultValue = 0;
-			};
-			class ServicingDrones
-			{
-				displayName = "Drones";
-				description = "Can Drones be Serviced here?";
-				typeName = "BOOL";
-				defaultValue = 0;
-			};*/
-		};
-		class ModuleDescription: ModuleDescription
-		{
-			description = "LRG Fundamentals Module Description Goes Here.";
-			sync[] = {"LocationArea_F"};
-			class LocationArea_F
-			{
-				position = 0;
-				optional = 0;
-				duplicate = 1;
-				synced[] = {"Anything"};
-			};
-		};
+		class ModuleDescription: ModuleDescription{};
 	};
-
+	class ModuleDescription: ModuleDescription
+	{
+		description[] = {
+			"Sync this module to an object to create a Repair Facility within that location.",
+			"The module will repair, refuel and rearm any vehicle after a set time delay.",
+			"This module is best used with Helipads/Parachute targets.",
+			"This Module can be synced to multiple objects or multiple modules can be used."
+		};
+			position = 0; // Position is taken into effect
+			direction = 0; // Direction is taken into effect
+			optional = 1; // Synced entity is optional
+			duplicate = 1; // Multiple entities of this type can be synced
+	};
+};
--- Addons/Vehicles/config.cpp
+++ Addons/Vehicles/config.cpp
@@ -21,8 +21,21 @@ class CfgVehicles
 	class Logic;
 	class Module_F: Logic
 	{
-		class ArgumentsBaseUnits;
-		class ModuleDescription;
+		class AttributesBase
+		{
+			class Default;
+			class Edit;
+			class Combo;
+			class Checkbox;
+			class CheckboxNumber;
+			class ModuleDescription;
+			class Units;
+		};
+
+		class ModuleDescription
+		{
+			class AnyBrain;
+		};
 	};
 	#include "ServicingModule.hpp"
 };
