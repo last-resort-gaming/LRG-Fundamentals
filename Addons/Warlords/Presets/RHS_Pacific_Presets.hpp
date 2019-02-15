56827462ccd9a2a3a28b41c5d2911077f135f10f
Mitch <cartwright1993@live.com>
2019-02-15 16:07:23 +0000

    Add RHS Faction Presets

---
+++ Addons/Warlords/Presets/RHS_Pacific_Presets.hpp
@@ -0,0 +1,595 @@
+	class LRG_RHS_Pacific
+	{
+		class WEST
+		{
+			class Infantry
+			{
+				class rhsusf_usmc_marpat_wd_rifleman_m4
+				{
+					cost=100;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_riflemanat
+				{
+					cost=300;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_grenadier
+				{
+					cost=125;
+					requirements[]={};
+				};
+				class rhsusf_navy_marpat_wd_medic
+				{
+					cost=125;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_autorifleman_m249
+				{
+					cost=150;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_explosives
+				{
+					cost=150;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_marksman
+				{
+					cost=150;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_combatcrewman
+				{
+					cost=150;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_driver
+				{
+					cost=150;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_engineer
+				{
+					cost=200;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_machinegunner
+				{
+					cost=200;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_helipilot
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_smaw
+				{
+					cost=300;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_stinger
+				{
+					cost=300;
+					requirements[]={};
+				};
+				class rhsusf_usmc_marpat_wd_sniper_M107
+				{
+					cost=300;
+					requirements[]={};
+				};
+			};
+			class Vehicles
+			{
+				class B_Quadbike_01_F
+				{
+					cost=100;
+					requirements[]={};
+				};
+				class rhsusf_m998_w_s_2dr
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class rhsusf_m1043_w_s
+				{
+					cost=350;
+					requirements[]={};
+				};
+				class rhsusf_M1078A1P2_B_WD_fmtv_usarmy
+				{
+					cost=650;
+					requirements[]={};
+				};
+				class rhsusf_M977A4_AMMO_usarmy_wd
+				{
+					cost=750;
+					requirements[]={};
+				};
+				class rhsusf_M978A4_usarmy_wd
+				{
+					cost=750;
+					requirements[]={};
+				};
+				class rhsusf_M1085A1P2_B_WD_Medical_fmtv_usarmy
+				{
+					cost=750;
+					requirements[]={};
+				};
+				class rhsusf_M977A4_REPAIR_usarmy_wd
+				{
+					cost=750;
+					requirements[]={};
+				};
+				class rhsusf_m1043_w_s_m2
+				{
+					cost=1000;
+					requirements[]={};
+				};
+				class rhsusf_m1045_w_s
+				{
+					cost=1250;
+					requirements[]={};
+				};
+				class rhsusf_M1232_MC_M2_usmc_wd
+				{
+					cost=1600;
+					requirements[]={};
+				};
+				class rhsusf_M1232_MC_MK19_usmc_wd
+				{
+					cost=1750;
+					requirements[]={};
+				};
+				class rhsusf_m113_usarmy_unarmed
+				{
+					cost=3000;
+					requirements[]={};
+				};
+				class RHS_M2A3_BUSKIII_wd
+				{
+					cost=3500;
+					requirements[]={};
+				};
+				class RHS_M6_wd
+				{
+					cost=4000;
+					requirements[]={};
+				};
+				class rhsusf_m1a1fep_wd
+				{
+					cost=5000;
+					requirements[]={};
+				};
+			};
+			class Aircraft
+			{
+				class RHS_MELB_MH6M
+				{
+					cost=1000;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class RHS_MELB_AH6M
+				{
+					cost=2500;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class RHS_UH1Y_UNARMED
+				{
+					cost=3000;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class RHS_UH1Y
+				{
+					cost=4000;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class rhsusf_CH53E_USMC
+				{
+					cost=4500;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class RHS_AH1Z_wd
+				{
+					cost=6000;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class RHS_A10
+				{
+					cost=7500;
+					requirements[]=
+					{
+						"A"
+					};
+				};
+				class RHS_C130J
+				{
+					cost=9000;
+					requirements[]=
+					{
+						"A"
+					};
+				};
+				class rhsusf_f22
+				{
+					cost=10000;
+					requirements[]=
+					{
+						"A"
+					};
+				};
+			};
+			class Naval
+			{
+				class B_Boat_Transport_01_F
+				{
+					cost=100;
+					requirements[]=
+					{
+						"W"
+					};
+				};
+				class rhsusf_mkvsoc
+				{
+					cost=750;
+					requirements[]=
+					{
+						"W"
+					};
+				};
+			};
+			class Gear
+			{
+				class rhsusf_mags_crate
+				{
+					cost=200;
+					requirements[]={};
+				};
+				class rhsusf_launcher_crate
+				{
+					cost=300;
+					requirements[]={};
+				};
+				class rhsusf_weapon_crate
+				{
+					cost=350;
+					requirements[]={};
+				};
+			};
+			class Defences
+			{
+				class RHS_M2StaticMG_D
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class RHS_M2StaticMG_MiniTripod_D
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class RHS_MK19_TriPod_D
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class RHS_Stinger_AA_pod_D
+				{
+					cost=500;
+					requirements[]={};
+				};
+				class RHS_TOW_TriPod_D
+				{
+					cost=500;
+					requirements[]={};
+				};
+			};
+		};
+		class EAST
+		{
+			class Infantry
+			{
+				class rhs_vmf_flora_rifleman
+				{
+					cost=100;
+					requirements[]={};
+				};
+				class rhs_vmf_flora_grenadier
+				{
+					cost=125;
+					requirements[]={};
+				};
+				class rhs_vmf_flora_grenadier_rpg
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class rhs_vmf_flora_medic
+				{
+					cost=125;
+					requirements[]={};
+				};
+				class rhs_msv_arifleman
+				{
+					cost=150;
+					requirements[]={};
+				};
+				class rhs_vmf_flora_marksman
+				{
+					cost=150;
+					requirements[]={};
+				};
+				class rhs_vmf_flora_armoredcrew
+				{
+					cost=150;
+					requirements[]={};
+				};
+				class rhs_vmf_flora_driver_armored
+				{
+					cost=150;
+					requirements[]={};
+				};
+				class rhs_vmf_flora_engineer
+				{
+					cost=200;
+					requirements[]={};
+				};
+				class rhs_vmf_flora_machinegunner
+				{
+					cost=200;
+					requirements[]={};
+				};
+				class rhs_pilot_combat_heli
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class rhs_vmf_flora_at
+				{
+					cost=300;
+					requirements[]={};
+				};
+				class rhs_vmf_flora_aa
+				{
+					cost=300;
+					requirements[]={};
+				};
+			};
+			class Vehicles
+			{
+				class O_Quadbike_01_F
+				{
+					cost=100;
+					requirements[]={};
+				};
+				class rhs_uaz_open_vdv
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class rhs_uaz_vdv
+				{
+					cost=350;
+					requirements[]={};
+				};
+				class rhs_gaz66o_vdv
+				{
+					cost=650;
+					requirements[]={};
+				};
+				class rhs_gaz66_ammo_vdv
+				{
+					cost=750;
+					requirements[]={};
+				};
+				class RHS_Ural_Fuel_VDV_01
+				{
+					cost=750;
+					requirements[]={};
+				};
+				class RHS_Ural_Repair_VDV_01
+				{
+					cost=750;
+					requirements[]={};
+				};
+				class rhs_tigr_m_3camo_vdv
+				{
+					cost=1000;
+					requirements[]={};
+				};
+				class rhs_tigr_sts_3camo_vdv
+				{
+					cost=1250;
+					requirements[]={};
+				};
+				class rhsgref_BRDM2UM_vdv
+				{
+					cost=1400;
+					requirements[]={};
+				};
+				class rhsgref_BRDM2_HQ_vdv
+				{
+					cost=1600;
+					requirements[]={};
+				};
+				class rhs_btr60_vdv
+				{
+					cost=3000;
+					requirements[]={};
+				};
+				class rhs_btr70_vdv
+				{
+					cost=3500;
+					requirements[]={};
+				};
+				class rhs_zsu234_aa
+				{
+					cost=4000;
+					requirements[]={};
+				};
+				class rhs_t80uk
+				{
+					cost=5000;
+					requirements[]={};
+				};
+				class rhs_t90a_tv
+				{
+					cost=6000;
+					requirements[]={};
+				};
+			};
+			class Aircraft
+			{
+				class RHS_Mi8mt_Cargo_vvsc
+				{
+					cost=2000;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class RHS_Ka52_vvsc
+				{
+					cost=3750;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class rhs_ka60_c
+				{
+					cost=3000;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class rhs_mi28n_vvsc
+				{
+					cost=6000;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class RHS_Mi24V_vvsc
+				{
+					cost=6500;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class RHS_Mi8mtv3_Cargo_vvsc
+				{
+					cost=4250;
+					requirements[]=
+					{
+						"H"
+					};
+				};
+				class RHS_Su25SM_vvsc
+				{
+					cost=7500;
+					requirements[]=
+					{
+						"A"
+					};
+				};
+				class rhs_mig29sm_vmf
+				{
+					cost=10000;
+					requirements[]=
+					{
+						"A"
+					};
+				};
+			};
+			class Naval
+			{
+				class O_Boat_Transport_01_F
+				{
+					cost=100;
+					requirements[]=
+					{
+						"W"
+					};
+				};
+			};
+			class Gear
+			{
+				class rhs_mags_crate
+				{
+					cost=200;
+					requirements[]={};
+				};
+				class rhs_launcher_crate
+				{
+					cost=300;
+					requirements[]={};
+				};
+				class rhs_weapon_crate
+				{
+					cost=350;
+					requirements[]={};
+				};
+			};
+			class Defences
+			{
+				class rhs_KORD_high_MSV
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class rhs_KORD_MSV
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class RHS_AGS30_TriPod_MSV
+				{
+					cost=250;
+					requirements[]={};
+				};
+				class rhs_Kornet_9M133_2_msv
+				{
+					cost=500;
+					requirements[]={};
+				};
+				class rhs_SPG9M_MSV
+				{
+					cost=500;
+					requirements[]={};
+				};
+				class rhs_Igla_AA_pod_msv
+				{
+					cost=500;
+					requirements[]={};
+				};
+				class RHS_ZU23_MSV
+				{
+					cost=500;
+					requirements[]={};
+				};
+			};
+		};
+	};
